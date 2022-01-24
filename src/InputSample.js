db443c5dcc227906a3e809a5bd6596b86c9d4299
taurusboong2 <taurusboong2@gmail.com>
2022-01-24 23:36:45 +0900

    multi input value handling

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,23 +1,37 @@
 import React, { useState } from 'react';
 
 function InputSample() {
-  const [text, setText] = useState('');
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: ''
+  });
+
+  const { name, nickname } = inputs;
 
   const onChange = (e) => {
-    setText(e.target.value);
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤,
+      [name] : value //name 키를 가진 값을 value로 설정
+    });
   }
 
   const onReset = () => {
-    setText('');
+    setInputs({
+      name: '',
+      nickname: ''
+    });
   }
 
 
   return (
     <div>
-      <input onChange={onChange} value={text} />
+      <input name='name' placeholder="이름" onChange={onChange} value={name} />
+      <input name='nickname' placeholder="닉네임" onChange={onChange} value={nickname} />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값: {text}</b>
+        <b>값: </b>
+        {name} ({nickname})
       </div>
     </div>
   );
