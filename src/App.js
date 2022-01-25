1329bd79aee99afe3485bdf559803ed1f5adf894
taurusboong2 <taurusboong2@gmail.com>
2022-01-25 20:32:04 +0900

    useMemo to optimization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useMemo } from 'react';
 import CreateUser from './CreateUser';
 import UserList from './UserList';
+
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는 중...');
+  return users.filter((user) => user.active).length;
+}
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -64,10 +69,13 @@ function App() {
     setUsers(users.map((user) => (user.id === id ? { ...user, active: !user.active } : user)));
   };
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
     <div>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수 : {count}</div>
     </div>
   );
 }
