c7fb56ec3bd33828376786566858768366b9d2e4
taurusboong2 <taurusboong2@gmail.com>
2022-01-25 18:30:15 +0900

    add element in array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import React from 'react';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import CreateUser from './CreateUser';
 import UserList from './UserList';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'lizy@example.com',
+    },
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+
+    setUsers(users.concat(user));
+    //위 코드는 concat을 통한 복사를 활용한 추가 방법.
+    //setUsers([...users, user]);
+    //위 코드는 spread 연산자를 활용한 추가 방법.
+
+    setInputs({
+      username: '',
+      email: '',
+    });
+    nextId.current += 1;
+  };
   return (
     <div>
-      <UserList />
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} />
     </div>
   );
 }
