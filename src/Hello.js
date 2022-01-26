ac8b1c650a2218ea4e9c57cef8fefd1631760400
taurusboong2 <taurusboong2@gmail.com>
2022-01-26 16:15:06 +0900

    add Immer & class component

--- src/Hello.js
+++ src/Hello.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({ name, color, bgc, isSpecial, binaryO }) {
-  return (
-    <div style={{
-      color : color,
-      backgroundColor : bgc
-    }}>
-      {isSpecial && <b>*</b>}
-      { binaryO ? <b>이것은 true </b> : <b>이것은 false </b>  }
-    안녕하세요 {name}
-    </div>
-  )
+class Hello extends Component {
+  static defaultProps = {
+    name: '이름없음',
+  };
+
+  render() {
+    const { color, name, isSpecial } = this.props;
+    return (
+      <div
+        style={{
+          color: color,
+        }}
+      >
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+      </div>
+    );
+  }
 }
 
-Hello.defaultProps = {
-  name: '이름없음'
-}
 
 export default Hello;
